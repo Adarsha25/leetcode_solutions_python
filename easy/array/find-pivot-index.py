e37eb72f2a66374c9cf755291d15ec6afd4d532f
Adarsha_S <s.adarsharao@gmail.com>
2022-12-18 13:31:58 +0530

    pivot index problem

---
+++ easy/array/find-pivot-index.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/find-pivot-index/
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+
+        leftsum = 0
+        for i in range(len(nums)):
+            rightsum = total - nums[i] - leftsum
+            if leftsum == rightsum:
+                return i
+            leftsum += nums[i]
+        return -1
