1b60e3ebbbd51e15647c21b27a0cff468dc3b586
Adarsha_S <s.adarsharao@gmail.com>
2022-12-26 14:14:11 +0530

    only sorting is enough

---
+++ medium/array/sort-colors.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/sort-colors/submissions/865660811/
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
