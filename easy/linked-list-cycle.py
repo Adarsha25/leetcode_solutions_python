d54901b7fd4251240ee13b17920e108d9eee8eec
Adarsha_S <s.adarsharao@gmail.com>
2023-01-11 23:57:20 +0530

    find if list is cyclic

---
+++ easy/linked-list-cycle.py
@@ -0,0 +1,19 @@
+#  https://leetcode.com/problems/linked-list-cycle/submissions/876295819/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        seen = set()
+        cur = head
+        while cur:
+            if cur in seen:
+                return True
+            seen.add(cur)
+            cur = cur.next
+        return False
+
