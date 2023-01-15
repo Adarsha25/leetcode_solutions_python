801216d49a7d067a529d9ada2a113b7dd4aa6765
Adarsha_S <s.adarsharao@gmail.com>
2023-01-15 22:20:58 +0530

    Valid parantheses

---
+++ easy/valid-parentheses.py
@@ -0,0 +1,15 @@
+#  https://leetcode.com/problems/valid-parentheses/submissions/878705321/
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        dict = {"]":"[", "}":"{", ")":"("}
+        for char in s:
+            if char in dict.values():
+                stack.append(char)
+            elif char in dict.keys():
+                if stack == [] or dict[char] != stack.pop():
+                    return False
+            else:
+                return False
+        return stack == []
