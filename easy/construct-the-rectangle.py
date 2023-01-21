7d23582ee4f41544a7e940506d90b4877afcce55
Adarsha_S <s.adarsharao@gmail.com>
2023-01-21 23:19:08 +0530

    forming the rectangle sides

---
+++ easy/construct-the-rectangle.py
@@ -0,0 +1,10 @@
+#  https://leetcode.com/problems/construct-the-rectangle/solutions/
+
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+    
+        mid = int(math.sqrt(area))
+        while mid > 0:
+            if area % mid == 0:
+                return [int(area / mid), int(mid)]
+            mid -= 1
