32c3008d0eeecaaa1749f40a82c6e8f44587bb4a
Adarsha_S <s.adarsharao@gmail.com>
2023-01-22 22:43:39 +0530

    reverse words in the string

---
+++ easy/reverse-words-in-a-string-iii.py
@@ -0,0 +1,16 @@
+#  https://leetcode.com/problems/reverse-words-in-a-string-iii/submissions/883189341/
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        res = ''
+        l, r = 0, 0
+        while r < len(s):
+            if s[r] != ' ':
+                r += 1
+            elif s[r] == ' ':
+                res += s[l:r + 1][::-1]
+                r += 1
+                l = r
+        res += ' '
+        res += s[l:r + 2][::-1]
+        return res[1:]
