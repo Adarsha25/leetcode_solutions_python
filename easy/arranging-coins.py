691e59e4e74bd8d61b4f87cc25774bcd9204631e
Adarsha_S <s.adarsharao@gmail.com>
2023-01-24 21:48:09 +0530

    arranging coins in stairway

---
+++ easy/arranging-coins.py
@@ -0,0 +1,9 @@
+#  https://leetcode.com/problems/arranging-coins/submissions/884457977/
+
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        rows = 1
+        while n > 0:
+            rows += 1
+            n -= rows
+        return rows - 1
